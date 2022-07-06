59082bda463bda102491757348536c2952ed8f44
abdulmojeed ayoola <mojeedayoola58@gmail.com>
2022-07-06 18:20:18 +0100

    reduce lines of code

--- script.js
+++ script.js
@@ -7,21 +7,15 @@ const score = document.querySelector(".score");
 let starScore = ""
 
 submit.addEventListener("click", function() {
+    if(!starScore) return
     container.classList.add("none");
     score.textContent = starScore;
     containerThankYou.classList.remove("none");
 });
 
-button.forEach(btn => btn.addEventListener("click", 
-function mjay(event) {
-    button.forEach(btn => {
-        btn.classList.remove("active")
-    });
-    if (event.target.classList.contains("button")) {
-        event.target.classList.add("active")
-    } else {
-        event.target.parentElement.classList.add("active")
+const all = document.addEventListener('click',(e)=>{
+    if(e.target.classList.contains('button')){
+        starScore = e.target.innerHTML
+        starScore.innerHTML = starScore
     }
-    starScore = event.target.textContent;
-}
-));
+})
